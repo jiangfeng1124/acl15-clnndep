1b2d94b227cbc4a2308c8895bceace72aad82f3f
jiangfeng1124 <jiangfeng1124@gmail.com>
2015-05-10 00:22:34 -0400

    training/test scripts

---
+++ test-CCA.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+tgt_lang=$1
+src=udt/en/
+tgt=udt/$tgt_lang/
+
+f_test=$tgt/$tgt_lang-universal-test-brown.conll
+f_output=$f_test.predict
+
+model=$src/model.cca.en-$tgt_lang/model
+f_conf=conf/nndep.cfg
+
+./nndep -cltest  $f_test \
+        -model $model \
+        -output $f_output \
+        -cfg $f_conf \
+        -clemb resources/cca/en-$tgt_lang/$tgt_lang.50.w2v
+
---
+++ train-PROJ.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# lang=${lang}
+corpus=udt/en/
+
+f_train=$corpus/en-universal-train-brown.conll
+f_dev=$corpus/en-universal-dev-brown.conll
+
+model_dir=$corpus/model.proj
+
+if [ ! -d $model_dir ]; then
+    mkdir $model_dir
+fi
+f_model=$model_dir/model
+f_conf=conf/nndep.cfg
+
+./bin/nndep -train $f_train \
+            -dev $f_dev     \
+            -model $f_model \
+            -cfg $f_conf    \
+            -emb resources/projected/en/en.50
+
